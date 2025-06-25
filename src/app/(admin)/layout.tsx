1bebbe47cd254988a4f2b468865d69057bc6c4e4
William <you@example.com>
2025-06-25 20:31:27 -0300

    Revert "feat: add shadcn sidebar component"
    This reverts commit 003088355547b729623ac031c2d0fcf29ac09a49.

--- src/app/(admin)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
-
-export default function AdminLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return <AdminPanelLayout>{children}</AdminPanelLayout>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import { ContentLayout } from "@/components/admin-panel/content-layout";
-
 export default function Home() {
 	return (
-		<ContentLayout title="72finn">
-			<main>
-				<h1>72finn</h1>
-				<p>Logar</p>
-			</main>
-		</ContentLayout>
+		<main>
+			<h1>72finn</h1>
+			<p>Logar</p>
+		</main>
 	);
 }
